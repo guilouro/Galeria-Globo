64636d35ca1830ca9ef669ac367dc33a3cdc79a7
Guilherme Louro <guilherme-louro@hotmail.com>
2014-08-14 20:17:33 -0300

    Init

---
+++ FRAMEWORK/app/Models/Admin_Model.php
@@ -0,0 +1,22 @@
+<?
+	class Admin_Model extends Model
+	{
+		public $_tabela = 'developer';
+
+		public $fk = array(
+			// 'categorias' => 'id_categorias',
+		);
+
+
+		public $orderby = array(
+			// 'titulo' => 'data DESC',
+		);
+
+
+		public $pagination = array(
+			'contato'	=> array('limit' => 5)
+		);
+
+	}
+
+?>
